5cb117ea24e9d02e907684162ee6979d141eebc1
AchiHachi <83038266+AchiHachi@users.noreply.github.com>
2021-09-05 04:12:21 +0300

    Update mainGUI.java

--- lab4.mirea.java/src/lab4/mirea/mainGUI.java
+++ lab4.mirea.java/src/lab4/mirea/mainGUI.java
@@ -1,5 +1,9 @@
 package lab4.mirea;
 
+// Laboratory work on the discipline "Programming in the Java language"
+// The work was carried out by a student of the INBO-09-20 group
+// The program was written by Babichev Ilia Igorevich
+
 public class mainGUI {
     public static void main(String[] args) {
         SimpleGUI app = new SimpleGUI();
