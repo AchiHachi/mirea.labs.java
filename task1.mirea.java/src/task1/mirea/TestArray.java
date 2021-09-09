ae339504d8eefef5f64623c2523c9cb972078132
AchiHachi <83038266+AchiHachi@users.noreply.github.com>
2021-09-09 18:32:17 +0300

    Update TestArray.java

--- task1.mirea.java/src/task1/mirea/TestArray.java
+++ task1.mirea.java/src/task1/mirea/TestArray.java
@@ -2,11 +2,15 @@ package task1.mirea;
 
 import java.util.Scanner;
 
+// Laboratory work on the discipline "Programming in the Java language"
+// The work was carried out by a student of the INBO-09-20 group
+// The program was written by Babichev Ilia Igorevich
+
 public class TestArray {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        System.out.println("Введите размер массива (от 2 по 5): ");
+        System.out.println("Enter the size of the array (2 to 5): ");
         int n = in.nextInt();
 
         ArrayWithLoop arr = new ArrayWithLoop(n);
