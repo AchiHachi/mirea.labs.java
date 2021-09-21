29c9135110935deed7dbab83fa569be19d12985c
AchiHachi <83038266+AchiHachi@users.noreply.github.com>
2021-09-22 01:03:22 +0300

    Update StudentTest.java

--- lab6.mirea.java/task1/StudentTest.java
+++ lab6.mirea.java/task1/StudentTest.java
@@ -2,6 +2,10 @@ package lab6.mirea;
 
 import java.util.Arrays;
 
+// Laboratory work on the discipline "Programming in the Java language"
+// The work was carried out by a student of the INBO-09-20 group
+// The program was written by Babichev Ilia Igorevich
+
 public class StudentTest {
     public static void main(String[] args) {
         Student[] arr = new Student[5];
