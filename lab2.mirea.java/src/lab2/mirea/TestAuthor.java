29e8dd5913365cde7643511df36ecf8fd685b6f8
AchiHachi <83038266+AchiHachi@users.noreply.github.com>
2021-09-22 00:49:44 +0300

    Update TestAuthor.java

--- lab2.mirea.java/src/lab2/mirea/TestAuthor.java
+++ lab2.mirea.java/src/lab2/mirea/TestAuthor.java
@@ -1,5 +1,9 @@
 package lab2.mirea;
 
+// Laboratory work on the discipline "Programming in the Java language"
+// The work was carried out by a student of the INBO-09-20 group
+// The program was written by Babichev Ilia Igorevich
+
 public class TestAuthor {
     public static void main(String[] args) {
         Author a1 = new Author("Sue Grant", 'm', "sugrant@somewhere.com");
