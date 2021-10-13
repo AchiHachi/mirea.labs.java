61095725a70279ef102b3d5d4f99c57b4d6c690f
AchiHachi <83038266+AchiHachi@users.noreply.github.com>
2021-10-13 22:34:15 +0300

    Update GameTest.java

--- task6.mirea.java/src/task6/GameTest.java
+++ task6.mirea.java/src/task6/GameTest.java
@@ -1,5 +1,9 @@
 package task6;
 
+// Laboratory work on the discipline "Programming in the Java language"
+// The work was carried out by a student of the INBO-09-20 group
+// The program was written by Babichev Ilia Igorevich
+
 public class GameTest {
     public static void main(String[] args) {
         GameClass game = new GameClass();
