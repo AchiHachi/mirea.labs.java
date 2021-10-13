e66dfa7358a19b007f036325a9aab3a5a3dd27d0
AchiHachi <83038266+AchiHachi@users.noreply.github.com>
2021-10-13 22:34:00 +0300

    Update TestMain.java

--- task5.mirea.java/src/task5/TestMain.java
+++ task5.mirea.java/src/task5/TestMain.java
@@ -1,5 +1,9 @@
 package task5;
 
+// Laboratory work on the discipline "Programming in the Java language"
+// The work was carried out by a student of the INBO-09-20 group
+// The program was written by Babichev Ilia Igorevich
+
 public class TestMain {
     public static void main(String[] args) {
         ShapeViewer app = new ShapeViewer();
