a90b64569b0a6cdd3e5703518bdd50084be68648
AchiHachi <83038266+AchiHachi@users.noreply.github.com>
2021-10-13 22:34:26 +0300

    Update ArrayListTest.java

--- task7.mirea.java/src/task7/ArrayListTest.java
+++ task7.mirea.java/src/task7/ArrayListTest.java
@@ -2,6 +2,10 @@ package task7;
 
 import java.util.ArrayList;
 
+// Laboratory work on the discipline "Programming in the Java language"
+// The work was carried out by a student of the INBO-09-20 group
+// The program was written by Babichev Ilia Igorevich
+
 public class ArrayListTest {
 
     public static void main(String[] args) {
