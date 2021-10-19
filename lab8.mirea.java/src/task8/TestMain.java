069c0238d69c6cb77f191fa1615eed889596e9ab
AchiHachi <83038266+AchiHachi@users.noreply.github.com>
2021-10-19 13:55:13 +0300

    Update TestMain.java

--- lab8.mirea.java/src/task8/TestMain.java
+++ lab8.mirea.java/src/task8/TestMain.java
@@ -3,6 +3,10 @@ package task8;
 import java.util.ArrayList;
 import java.util.List;
 
+// Laboratory work on the discipline "Programming in the Java language"
+// The work was carried out by a student of the INBO-09-20 group
+// The program was written by Babichev Ilia Igorevich
+
 public class TestMain {
     public static void main(String[] args) {
         WaitList<Integer> lst1 = new BoundedWaitList<Integer>(5);
