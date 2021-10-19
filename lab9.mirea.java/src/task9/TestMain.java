027c142c671f4b93cd908b3b527d801ca8656fa6
AchiHachi <83038266+AchiHachi@users.noreply.github.com>
2021-10-19 15:15:17 +0300

    Update TestMain.java

--- lab9.mirea.java/src/task9/TestMain.java
+++ lab9.mirea.java/src/task9/TestMain.java
@@ -1,5 +1,9 @@
 package task9;
 
+// Laboratory work on the discipline "Programming in the Java language"
+// The work was carried out by a student of the INBO-09-20 group
+// The program was written by Babichev Ilia Igorevich
+
 public class TestMain {
     public static void main(String[] args) {
         Account acc1 = new Account("364432", "Андрей");
