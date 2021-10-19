3234770c542b546598b157a6e868fcbe87f93ebe
AchiHachi <83038266+AchiHachi@users.noreply.github.com>
2021-10-19 15:13:59 +0300

    Delete lab9.mirea.java directory

--- lab9.mirea.java/src/task9/Verification.java
+++
@@ -1,8 +0,0 @@
-package task9;
-
-public class Verification  extends Exception{
-    public Verification(Account client){
-        super("Имя: " + client.getName() + " \nИНН: " + client.getInn() + " - недействителен\n");
-    }
-
-}
